d362c82d1583b6a7e1bf416604b1b908b637d3b3
ManveerSingh0 <singhcoder2005@gmail.com>
2025-08-21 12:21:50 +0530

    added some features

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,3 +5,10 @@
 
 
 (load-theme 'manoj-dark t)
+
+
+
+(keymap-global-set "C-c ;" 'windmove-right) 
+(keymap-global-set "C-c j" 'windmove-left)
+(keymap-global-set "C-c k" 'windmove-down)
+(keymap-global-set "C-c l" 'windmove-up)
